b83aed6afdd84e4e5858970796a658e24a0aad63
nayan911 <nayanagarwal911@gmail.com>
2025-06-08 17:39:55 +0530

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const db = require('./sql');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
